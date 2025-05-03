25fd6ac0aae4f8112d694a28c66d81b8c3b709a2
Daniel Pittard <126673498+PittsGitHub@users.noreply.github.com>
2025-05-03 23:36:21 +0100

    test: husky and lint-staged in action

---
+++ eslint.config.cjs
@@ -0,0 +1,38 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+const reactHooks = require('eslint-plugin-react-hooks')
+const reactRefresh = require('eslint-plugin-react-refresh')
+const tseslint = require('typescript-eslint')
+const prettier = require('eslint-plugin-prettier')
+const tsParser = require('@typescript-eslint/parser') // ← actual parser object
+
+module.exports = tseslint.config(
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'warn',
+    },
+  }
+)
